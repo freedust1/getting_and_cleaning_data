7f21354d4038e597749185a388ab443445824935
Donghun Lee <freedust1@gmail.com>
2016-06-20 14:41:38 +0900

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -15,3 +15,21 @@ SBJ <- rbind(subject_train, subject_test)
 
 features <- read.table("features.txt")
 activity_labels <- read.table("activity_labels.txt")
+
+# extract only mean and std measurement
+mean_std <- grep("-(mean|std)\\(\\)", features[,2])
+X <- X[, mean_std]
+
+# give activity labels to Y
+Y[,1] <- activity_labels[Y[,1],2]
+
+# give column name
+names(X) <- features[mean_std, 2]
+names(Y) <- "activity_labels"
+names(SBJ) <- "subjects"
+
+# bind all the data
+bind_data <- cbind(X, Y, SBJ)
+
+# write result dataset to file
+write.table(result_dataset, "dataset.txt", row.name=FALSE)
